0dec35a472fcb84d844616ebba91e6687c1a08c6
Marivatis <cyvax2@gmail.com>
2023-12-16 17:24:04 +0200

    Complete query 6

--- Program.cs
+++ Program.cs
@@ -79,7 +79,7 @@ namespace Practice_Linq
 
             var selectedGames = games
                 .Where(game => 
-                    game.Neutral &&
+                    game.Tournament.Equals("Friendly") &&
                     (game.Home_team.Equals("Italy") || game.Away_team.Equals("Italy")) &&
                     game.Date.Year >= 2020); // Корегуємо запит !!!
 
@@ -203,14 +203,28 @@ namespace Practice_Linq
             //Query 6: Вивести всі матчі останнього чемпіоната світу з футболу (FIFA World Cup), починаючи з чвертьфіналів (тобто останні 8 матчів).
             //Матчі мають відображатися від фіналу до чвертьфіналів (тобто у зворотній послідовності).
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games
+                .Where(game => game.Tournament.Equals("FIFA World Cup"))
+                .TakeLast(8)
+                .OrderByDescending(p => p.Date);   // Корегуємо запит !!!
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 6 ========================");
 
             // див. приклад як має бути виведено:
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine(ToString(game));
+            }
 
+            static string ToString(FootballGame game)
+            {
+                return game.Date.ToString("dd.MM.yyyy") +
+                    $" {game.Home_team} - {game.Away_team}, " +
+                    $"Score: {game.Home_score} - {game.Away_score}, " +
+                    $"Country: {game.Country}";
+            }
 
         }
 
